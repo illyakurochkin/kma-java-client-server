ff6972b962120688f0227d01adbf349b23bee3ee
illyakurochkin <illya.kurochkin@gmail.com>
2019-05-27 01:55:57 +0300

    Change constants in CommandTypes

--- src/main/java/server/CommandTypes.java
+++ src/main/java/server/CommandTypes.java
@@ -1,8 +1,7 @@
 package server;
 
 public class CommandTypes {
-    public static final int CHECK_AMOUNT = 1;
-    public static final int GET = 2;
+    public static final int CHECK = 1;
+    public static final int REMOVE = 2;
     public static final int PUT = 3;
-    public static final int CREATE_GROUP = 4;
 }
