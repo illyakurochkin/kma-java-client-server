3cc8278fe683bcb3de646412e8afa53fa898532f
illyakurochkin <illya.kurochkin@gmail.com>
2019-05-27 01:57:33 +0300

    Get SecretKey from constructor

--- src/main/java/server/FakeReceiver.java
+++ src/main/java/server/FakeReceiver.java
@@ -1,12 +1,9 @@
 package server;
 
 import protocol.Package;
-import protocol.PackageCreator;
 import protocol.PackageParser;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Random;
 
@@ -15,36 +12,14 @@ public class FakeReceiver implements Receiver {
     private SecretKey secretKey;
     private Processor processor;
 
-    public FakeReceiver(Processor processor, int commandAmount) {
-        try {
-            KeyGenerator key = KeyGenerator.getInstance("AES");
-            key.init(256);
-            secretKey = key.generateKey();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
+    public FakeReceiver(SecretKey secretKey, Processor processor, List<byte[]> bytes) {
+        this.secretKey = secretKey;
         this.processor = processor;
-        startFakeReceiving(commandAmount);
-    }
-
-    private void startFakeReceiving(int commandAmount) {
-        PackageCreator creator = new PackageCreator();
-
-        List<byte[]> bytes = List.of(creator.create((byte) 1, 1, 1, "first", secretKey),
-                creator.create((byte) 1, 1, 1, "second", secretKey),
-                creator.create((byte) 1, 1, 1, "third", secretKey));
-
-        for (int i = 0; i < commandAmount; ) {
-            for (int j = 0; j < bytes.size() && i < commandAmount; j++, i++) {
-                receive(bytes.get(j));
-            }
-        }
+        bytes.forEach(this::receive);
     }
 
     @Override
     public void receive(byte[] byteArray) {
-
         new Thread(() -> {
             try {
                 Thread.sleep(new Random().nextInt(400) + 20);
