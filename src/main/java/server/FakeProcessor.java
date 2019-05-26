cd493f08ffc23244a073e4f03df1919751e537bb
illyakurochkin <illya.kurochkin@gmail.com>
2019-05-27 01:57:04 +0300

    Refactor FakeProcessor to store data

--- src/main/java/server/FakeProcessor.java
+++ src/main/java/server/FakeProcessor.java
@@ -1,18 +1,18 @@
 package server;
 
-import model.Product;
 import protocol.Package;
 
+import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 
 public class FakeProcessor implements Processor {
 
-    private Set<Product> database;
+    private final Map<String, Integer> database;
     private Sender sender;
 
-    public FakeProcessor(Sender sender) {
+    public FakeProcessor(Sender sender, Map<String, Integer> database) {
         this.sender = sender;
+        this.database = database;
     }
 
     @Override
@@ -24,6 +24,26 @@ public class FakeProcessor implements Processor {
                 // make database connection
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }
+
+            String productName;
+            int amount;
+            switch (pack.getcType()) {
+                case CommandTypes.REMOVE:
+                    productName = pack.getMessage().split(" ")[0].toLowerCase();
+                    amount = Integer.parseInt(pack.getMessage().split(" ")[1]);
+                    database.put(productName, database.getOrDefault(productName, 0) + -amount);
+                    break;
+                case CommandTypes.PUT:
+                    productName = pack.getMessage().split(" ")[0].toLowerCase();
+                    amount = Integer.parseInt(pack.getMessage().split(" ")[1]);
+                    database.put(productName, database.getOrDefault(productName, 0) + amount);
+                    break;
+                case CommandTypes.CHECK:
+                    productName = pack.getMessage().split(" ")[0].toLowerCase();
+                    amount = database.getOrDefault(productName, 0);
+                    sender.send((productName + ":" + amount).getBytes());
+                    return;
             }
             sender.send("OK".getBytes());
         }).start();
